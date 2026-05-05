4c154de60193910700220edc0a4035f0e1395eaf
janvi-jain <pariijain007@gmail.com>
2026-05-05 20:56:59 +0530

    completed portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,62 @@
 function Contact() {
+
+  const navItems = [
+    {id: 1, name: "Location", icon: <i class="ri-map-pin-2-fill"></i>, value: "Surat,Gujarat" },
+    {id: 2, name: "Phone", icon: <i class="ri-phone-fill"></i>, value: "+91 94290 62463" },
+    {id: 3, name: "Email", icon: <i class="ri-mail-fill"></i>, value: "Pariijain007@gmail.com" },
+  ];
+
   return (
     <section id="contact" className="mt-105">
+      <p className="title text-center fs-4 fw-normal">Contact Me</p>
+      <h2 className="edu text-center display-2 fw-bold mb-xl-2 mb-4">Let’s Start A New Project</h2>
       <div className="container">
-        <h2>Contact</h2>
+        <div className="row">
+          <div className="col-lg-6">
+            {navItems.map((item) => (
+              <div className="contact-details d-flex mb-2" key={item.id}>
+                <div className="contact-img mb-3">
+                  {item.icon}
+                </div>
+                <div className="flex-column ms-4">
+                  <h4 className="fw-bold fs-4">{item.name}</h4>
+                  <p className="text-secondary fs-6 fw-normal">{item.value}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="col-lg-6">
+            <form className="row g-3 mt-xl-0 mt-md-4 mt-3">
+              <div className="col-md-6" data-aos="fade-up" data-aos-duration="2000">
+                <input type="email" className="form-control input-text" id="inputEmail4"
+                  placeholder="Your Name" />
+              </div>
+              <div className="col-md-6" data-aos="fade-up" data-aos-duration="2000">
+                <input type="password" className="form-control input-text" id="inputPassword4"
+                  placeholder="Your Email" />
+              </div>
+              <div className="col-md-6" data-aos="fade-up" data-aos-duration="2000">
+                <input type="email" className="form-control input-text" id="inputEmail4"
+                  placeholder="your Phone" />
+              </div>
+              <div className="col-md-6" data-aos="fade-up" data-aos-duration="2000">
+                <input type="password" className="form-control input-text" id="inputPassword4"
+                  placeholder="Your Subject" />
+              </div>
+              <div className="col-12" data-aos="fade-up" data-aos-duration="2000">
+                <textarea id="message" name="message" rows="5" cols="50" className="form-control input-text"
+                  placeholder="Start writing message here"></textarea>
+              </div>
+              <div className="col-12" data-aos="fade-up" data-aos-duration="2000">
+                <button type="submit" className="btn submit-btn fw-bold">submit now</button>
+              </div>
+            </form>
+          </div>
+          <iframe className="mt-5" data-aos="fade-up" data-aos-duration="3000"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238132.67288580257!2d72.65746993675346!3d21.159440568354555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04e59411d1563%3A0xfe4558290938b042!2sSurat%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1753163538056!5m2!1sen!2sin"
+            width="800" height="300" style={{ border: "0" }} loading="lazy" allowFullScreen frameBorder="0" referrerPolicy="no-referrer-when-downgrade">
+          </iframe>
+        </div>
       </div>
     </section>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,62 @@
+import logo from "/src/assets/images/janvi-logo.png";
+import instagram from "/src/assets/images/instagram.png";
+import twitter from "/src/assets/images/twitter.png";
+import github from "/src/assets/images/github-logo.png";
+import linkedin from "/src/assets/images/linkedin.png";
+
 function Footer() {
 
   const icons = [
-    "ri-instagram-line",
-    "ri-twitter-fill",
-    "ri-linkedin-fill",
-    "ri-github-fill"
+    { img: instagram, name: "Instagram" },
+    { img: twitter, name: "Twitter" },
+    { img: github, name: "GitHub" },
+    { img: linkedin, name: "LinkedIn" },
   ];
 
   return (
     <footer>
-      <div className="container text-center">
+      <div class="container">
+        <div class="row gy-lg-5 gy-3">
+          <div class="col-lg-4 col-sm-6 order-1">
+            <div>
+              <div class="logo">
+                <img src="" alt="janvi logo" width="70" height="60" class="mb-4" />
+                <p class="copyright text-secondary fw-normal fs-6">
+                  All rights reserved <span class="fw-bold">Codeefly</span> © 2025
+                </p>
+              </div>
+            </div>
+          </div>
 
-        <ul className="d-flex justify-content-center gap-3">
-          {icons.map((icon, i) => (
-            <li key={i}>
-              <i className={icon}></i>
-            </li>
-          ))}
-        </ul>
+          <div class="col-lg-4 col-sm-6 order-lg-3 order-2">
+            <div>
+              <h4 class="fw-bold fs-3 mb-3 follow">Follow Me</h4>
+              <ul class="list-unstyled d-flex gap-3 text-white fs-5 fw-bold icons">
+                {icons.map((item) => (
+                  <li><img src={item.img} alt={item.name} /></li>
+                ))}
+              </ul>
+            </div>
+          </div>
 
-        <p>© 2025 Janvi</p>
+          <div class="col-lg-4  order-lg-2 order-3">
+            <div>
+              <h4 class="fw-bold fs-3 mb-3 newsletter">Newsletter</h4>
+              <div class="row mb-3">
+                <div class="col-sm-10">
+                  <input type="email" class="form-control EMAIL-FORM" id="colFormLabel"
+                    placeholder="Submit your email" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div class="toparrow">
+        <a href="#home" class="position-fixed bottom-0 end-0">
+          <i class="ri-arrow-up-double-line"></i>
+        </a>
       </div>
     </footer>
   );
